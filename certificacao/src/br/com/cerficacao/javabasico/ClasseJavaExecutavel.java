6b282a5081dabce7c34539f4ede71d4c5c6d27ca
Everson Santos <everson.lisboa.santos@gmail.com>
2016-12-13 17:44:07 -0200

    Estude Escopo de variavel, estrutura de classe

---
+++ certificacao/src/br/com/cerficacao/javabasico/ClasseJavaExecutavel.java
@@ -0,0 +1,13 @@
+package br.com.cerficacao.javabasico;
+
+public class ClasseJavaExecutavel {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Olá mundo");
+
+	}
+
+}
---
+++ certificacao/src/br/com/cerficacao/javabasico/EstruturaClasse.java
@@ -0,0 +1,5 @@
+package br.com.cerficacao.javabasico;
+
+public class EstruturaClasse {
+
+}
