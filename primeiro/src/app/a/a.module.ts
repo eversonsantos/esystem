7d51b04f553a174c069ab776989601db42a564e4
Everson Santos <everson.lisboa.santos@gmail.com>
2017-06-12 16:34:44 -0300

    Compartilhando projeto

---
+++ primeiro/src/app/a/a.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AComponent } from './a.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AComponent]
+})
+export class AModule { }
